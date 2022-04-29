505da391a2239829fa3e8fc641d5a4d6c9b85e87
Nahum Gamaliel Santana <37852973+LordFitoi@users.noreply.github.com>
2022-04-29 14:27:16 -0400

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,7 +85,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "feline.users.apps.UsersConfig",
-    "feline.jobposts.apps.JobpostsConfig"
+    "feline.jobposts.apps.JobpostsConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
